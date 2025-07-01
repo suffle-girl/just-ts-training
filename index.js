73f7704febaec5e1c588d009a549f2983bf17dda
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-01 09:52:29 +0200

    numberToString

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("i work truly!");
+var numberToString = function (num) {
+    return String(num);
+};
+console.log("numberToString: ", numberToString(6), "type: ", typeof numberToString(6));
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("i work truly!");
+const numberToString = (num: number): string => {
+  return String(num);
+};
+console.log(
+  "numberToString: ",
+  numberToString(6),
+  "type: ",
+  typeof numberToString(6)
+);
