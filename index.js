9746cd58ce3835e0820cdd11187b07dc497d152f
Šárka Flídrová <sarka.flidrova@email.cz>
2025-07-02 07:58:59 +0200

    zeroFuel

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ export var growLoop = function (arr) {
 export var grow = function (arr) {
     return arr.reduce(function (acc, curr) { return acc * curr; }, 1);
 };
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+export var zeroFuelIf = function (distance, mpg, fuelLeft) {
+    if (distance <= mpg * fuelLeft) {
+        return true;
+    }
+    return false;
+};
+export var zeroFuel = function (distance, mpg, fuelLeft) {
+    return distance <= mpg * fuelLeft;
+};
